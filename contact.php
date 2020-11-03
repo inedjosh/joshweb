911d8310155af8160d0faf4f8e4fb101f3e43a5e
inedjosh <64702730+inedjosh@users.noreply.github.com>
2020-11-03 06:32:02 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
-    link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
     <link id="theme-style" rel="stylesheet" type="text/css" href="">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="" href="">
