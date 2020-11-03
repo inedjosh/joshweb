d970cbe4735d827baa70061df8cff34094fed025
inedjosh <64702730+inedjosh@users.noreply.github.com>
2020-11-03 09:46:03 +0100

    Update form.js

--- form.js
+++ form.js
@@ -12,12 +12,5 @@ function clearError(){
   alert.remove();
 }
 
-const alert1 = document.querySelector('.alert-div1');
-
-setTimeout(clearError, 3000);
 
 
-// Clear error
-function clearError(){
-alert1.remove();
-}
