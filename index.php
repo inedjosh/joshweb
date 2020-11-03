0061c8cfea27a9dc287fd8c29d26f4b432781a1f
inedjosh <64702730+inedjosh@users.noreply.github.com>
2020-11-03 10:44:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -170,7 +170,7 @@ Warm regards.
 	</section> 
     <footer class="s1 footer">
         
-        <p>copyright &copy; Inedu Joshua 2020</p>
+        <p> &copy; Inedu Joshua 2020</p>
     </footer>
 	<script type="text/javascript" src="main.js"></script>
 	<script src="form.js"></script>
